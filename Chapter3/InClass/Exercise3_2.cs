a00b504cf85544eeb956183a80ded6429a981cc1
Brendan Lienau <380790@colum.edu>
2021-04-05 18:26:02 -0500

    Chapter3 Inclass exercises

---
+++ Chapter3/InClass/Exercise3_2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise3_2 : MonoBehaviour
+{
+    public GameObject Cannonball;
+
+    public float shotForce = 100f;
+    public float torque = 10f;
+    public float reloadTime = 5f;
+   
+    float timer;
+    void Start()
+    {
+        timer = reloadTime;
+    }
+
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        else
+        {
+            timer = reloadTime;
+            FireCannonball();
+        }
+    }
+
+    void FireCannonball()
+    {
+        GameObject tempBall = Instantiate(Cannonball, this.transform);
+        tempBall.GetComponent<Rigidbody>().AddForce((Vector3.up + Vector3.forward) * shotForce, ForceMode.Impulse);
+        tempBall.GetComponent<Rigidbody>().AddTorque(Vector3.right * torque, ForceMode.Impulse);
+    }
+}
